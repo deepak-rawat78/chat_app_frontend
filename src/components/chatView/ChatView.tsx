10d4dbb3f944af9578c382983ab7b38c8fde5a8a
deepak-rawat78 <deepakrawat7789@gmail.com>
2023-08-12 23:25:42 +0530

    chat view UI updated

--- src/components/chatView/ChatView.tsx
+++ src/components/chatView/ChatView.tsx
@@ -1,18 +1,45 @@
 import React from "react";
 import { Props } from "./typings";
 import Message from "../message";
+import "./chatView.css";
 
 const ChatView = (props: Props) => {
 	const {} = props;
 	return (
 		<div className="chat-container">
-			<div className="message-container">
+			<div className="chat-message--container">
+				<Message value="das" isOwnMessage={true} />
+				<Message value="das" isOwnMessage={false} />
 				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfj"
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
 					isOwnMessage={true}
 				/>
 				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfj"
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={false}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={true}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={true}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={false}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={true}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={true}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
 					isOwnMessage={false}
 				/>
 			</div>
