bf6a985cf76accc145877a43c230061e3f8f1632
deepak-rawat78 <deepakrawat7789@gmail.com>
2023-08-12 22:18:40 +0530

    chat view created

--- src/components/chatView/ChatView.tsx
+++ src/components/chatView/ChatView.tsx
@@ -5,15 +5,29 @@ import Message from "../message";
 const ChatView = (props: Props) => {
 	const {} = props;
 	return (
-		<div className="h-52 w-1/2 border-2  ">
-			<Message
-				value="ds kjflksdj fklsdjk fjksjd fklsjdfj"
-				isOwnMessage={true}
-			/>
-			<Message
-				value="ds kjflksdj fklsdjk fjksjd fklsjdfj"
-				isOwnMessage={false}
-			/>
+		<div className="chat-container">
+			<div className="message-container">
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={true}
+				/>
+				<Message
+					value="ds kjflksdj fklsdjk fjksjd fklsjdfj"
+					isOwnMessage={false}
+				/>
+			</div>
+			<div className="chat-input-container">
+				<input
+					type="text"
+					className="chat-input-text"
+					aria-multiline="true"
+				/>
+				<input
+					type="submit"
+					className="chat-input-button"
+					value="Send"
+				/>
+			</div>
 		</div>
 	);
 };
