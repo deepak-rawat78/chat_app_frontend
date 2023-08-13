5438c716270f69375b1c18e6d847cc05cd759dbe
deepak-rawat78 <deepakrawat7789@gmail.com>
2023-08-13 13:08:39 +0530

    chat view updated with some dummy data

--- src/components/chatView/ChatView.tsx
+++ src/components/chatView/ChatView.tsx
@@ -1,64 +1,92 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Props } from "./typings";
 import Message from "../message";
 import "./chatView.css";
+import useCurrentUser from "@/lib/useCurrentUser";
+import SignalingService from "@/utils/SignalingService";
 
 const ChatView = (props: Props) => {
 	const {} = props;
+	const { user, logout } = useCurrentUser();
+	const senderUserName =
+		user?.data?.userName !== "deepak08" ? "deepak07" : "deepak08";
+	const [messageText, setMessageText] = useState("");
+	const [messageList, setMessageList] = useState([]);
+	const [messageService, setMessageService] =
+		useState<SignalingService | null>(null);
+
+	let messageRef = useRef([]).current;
+
+	useEffect(() => {
+		if (user?.data?.userName) initMessageService();
+	}, [user?.data?.userName]);
+
+	const handleOnChangeMessage = (e) => {
+		e.preventDefault();
+		console.log(e.target.value);
+
+		setMessageText(e.target.value);
+	};
+
+	const handleClickOnSendButton = (e) => {
+		e.preventDefault();
+		sendMessage();
+	};
+
+	const getMessageListener = (data) => {
+		const list = [...messageRef, data];
+		setMessageList(list);
+		messageRef = list;
+	};
+
+	const initMessageService = () => {
+		let temp = new SignalingService(user?.data?.userName);
+		temp.connect();
+
+		temp.getMessage(getMessageListener);
+		setMessageService(temp);
+	};
+
+	const sendMessage = () => {
+		messageService?.sendMessage({
+			sendTo: user.data.userName === "deepak08" ? "deepak07" : "deepak08",
+			from: user.data.userName,
+			message: messageText,
+		});
+	};
+
 	return (
 		<div className="chat-container">
 			<div className="chat-header">
 				<div className="chat-header-inner">
-					<p className="chat-header--username">deepak-07</p>
+					<p className="chat-header--username">
+						{user?.data?.userName}
+					</p>
 					<p className="chat-header--status">Active</p>
 				</div>
 			</div>
 			<div className="chat-message--container">
-				<Message value="das" isOwnMessage={true} />
-				<Message value="das" isOwnMessage={false} />
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={true}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={false}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={true}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={true}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={false}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={true}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={true}
-				/>
-				<Message
-					value="ds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfjds kjflksdj fklsdjk fjksjd fklsjdfj"
-					isOwnMessage={false}
-				/>
+				{messageList.map((value: any, index: number) => (
+					<Message
+						key={index}
+						from={value.from}
+						value={value.message}
+						isOwnMessage={senderUserName === value.from}
+					/>
+				))}
 			</div>
 			<div className="chat-input-container">
-				<span
-					className="input chat-input-text"
-					role="textbox"
-					contentEditable
-				></span>
 				<input
-					type="submit"
+					type="text"
+					className="chat-input-text"
+					value={messageText}
+					onChange={handleOnChangeMessage}
+				/>
+				<button
+					type="button"
 					className="chat-input-button"
 					value="Send"
+					onClick={handleClickOnSendButton}
 				/>
 			</div>
 		</div>
